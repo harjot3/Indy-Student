2879859dbaf2aa709fc3aa71f229713d9026b172
harjotsingh5 <hsingh79@ivytech.edu>
2025-03-26 14:21:57 -0400

    creating website

---
+++ myscript.js
@@ -0,0 +1,9 @@
+document.getElementById("first_js").innerHTML = "My First JavaScript";
+
+function fun() {
+    document.getElementById("first_js").innerHTML = "Big ahh dih 💔";
+}
+
+function apple() {
+    document.getElementById("d1").innerHTML = "Veinny ahh Dih 💔";
+}
